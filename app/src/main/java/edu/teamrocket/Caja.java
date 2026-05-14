31d8be5ece1cceeb5f68ee863da6192ea7be6e23
Sara <saragarciagarrido96@gmail.com>
2026-05-14 10:21:33 +0200

    build: Actualizar UML
    Co-authored-by: T0T11 tomas.orellano@gmail.com
    
    Co-authored-by: T0T11 tomas.orellano@gmail.com

---
+++ app/src/main/java/edu/teamrocket/Caja.java
@@ -0,0 +1,12 @@
+package edu.teamrocket;
+
+public class Caja implements Packing {
+
+    public Caja() {};
+
+    @Override
+    public String envoltorio() {
+        return "Caja";
+    };
+    
+}
---
+++ app/src/main/java/edu/teamrocket/Tubo.java
@@ -0,0 +1,11 @@
+package edu.teamrocket;
+
+public class Tubo implements Packing {
+
+    public Tubo() {};
+
+    @Override
+    public String envoltorio() {
+        return "Tubo";
+    };
+}
